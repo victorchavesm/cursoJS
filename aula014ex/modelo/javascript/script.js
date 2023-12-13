e670ded13143c361b4b0a49c4f1deded43ba8f12
Victor Chaves <104216761+victorchavesm@users.noreply.github.com>
2023-12-13 12:21:06 -0300

    exercicios

--- aula014ex/modelo/javascript/script.js
+++ aula014ex/modelo/javascript/script.js
@@ -5,8 +5,26 @@ function contar() {
     let resultado = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar'
     } else {
-        resultado.innerHTML = 'Contando'
+        resultado.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if( p <= 0) {
+            alert('Passo inválido. Segue a contagem considerando Passo = 1')
+            p = 1
+        }
+        if (i < f) {
+            for(let c = i; c <= f; c += p) {
+                resultado.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            for(let c = i; c >= f; c -= p) {
+                resultado.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        resultado.innerHTML += ` \u{1F3C1}`
+        
     }
 }
