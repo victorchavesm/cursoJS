d4866ae8e8a81bd4f2b4954521ec72fdb917dc13
Victor Chaves <104216761+victorchavesm@users.noreply.github.com>
2023-12-17 21:35:04 -0300

    exercicios

---
+++ aula015 - Variaveis Compostas/ex001.js
@@ -0,0 +1,18 @@
+let num = [5, 8, 9, 3, 10, 12, 13, 11, 10]
+console.log(num)
+console.log(`Teste ${num}`)
+console.log(num[9])
+
+
+
+num[3] = 1000
+console.log(num)
+num.push(32323232)
+console.log(`TESTE ${num}`)
+
+
+// comprimento do array: num.length
+console.log(num.length)
+
+console.log(num.sort())
+num.sort()
