7b9aa8aec9e1955d69462cb676225c1337c31e1b
victorchavesm <victorhcmnz@gmail.com>
2023-12-19 14:31:02 -0300

    exercicio

--- aula16ex/Analisador/javascript/script.js
+++ aula16ex/Analisador/javascript/script.js
@@ -11,7 +11,7 @@ function isNumero(n) {
     }
 }
 
-function inLista() {
+function inLista(n, l) {
     if (l.indexOf(Number(n)) != -1) {
         return true
     } else {
@@ -21,9 +21,34 @@ function inLista() {
 
 
 function adicionar(){
-    if (isNumero(num.value) && !inLista(num.value)) {
-
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0){
+        alert('Adicione valores antes de finalizar.')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+
+        for(let pos in valores) {
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if (valores[pos] < menor)
+            menor = valores[pos]
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados. </p>`
     }
 }
