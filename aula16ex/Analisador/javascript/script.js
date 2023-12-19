a6878d4cdd7240ccfcdb827731d6f52c1047093c
victorchavesm <victorhcmnz@gmail.com>
2023-12-19 14:41:54 -0300

    exercicios

--- aula16ex/Analisador/javascript/script.js
+++ aula16ex/Analisador/javascript/script.js
@@ -41,14 +41,23 @@ function finalizar() {
         let tot = valores.length
         let maior = valores[0]
         let menor = valores[0]
+        let soma = 0
+        let media = 0
 
         for(let pos in valores) {
+            soma += valores[pos]
             if (valores[pos] > maior)
             maior = valores[pos]
             if (valores[pos] < menor)
             menor = valores[pos]
         }
+        media = soma / tot
+
         res.innerHTML = ''
-        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados. </p>`
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados. </p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
     }
 }
